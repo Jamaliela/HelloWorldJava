9c4ad6afd952be74dbf3b8ab47745638ba8786ff
Ela <jamaliela@github.com>
2020-05-20 11:50:31 -0400

    initil commit

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -1,7 +1,1 @@
-package com.example.helloworld;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World !!");
-    }
-}
+package com.example.helloworld;public class HelloWorld {    public static void main(String[] args) {        System.out.println("Hello World !");    }}
